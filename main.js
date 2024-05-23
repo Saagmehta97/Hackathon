a0d351a3b608ffbfbc15eebcb7cfd137ec7994bf
Piero Espejo <Piero914@gmail.com>
2024-05-23 14:31:11 -0400

    get list working

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
 // asynchronous fetching of data possibly? Depends on our chrome extension
 async function getSportsData() {
   const url =
-    'https://allsportsapi2.p.rapidapi.com/api/motorsport/team/191417/stage/season/188803/races';
+    'https://allsportsapi2.p.rapidapi.com/api/motorsport/stage/203968/standings/team';
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': 'aeb566f723mshae7a1cef8cccfbbp1aa0b2jsnce7dca99c57f',
-      'X-RapidAPI-Host': 'allsportsapi2.p.rapidapi.com',
+      'x-rapidapi-key': 'e95d8113b4msh8b1b196c4cd13c4p113f3fjsn24b346e193d1',
+      'x-rapidapi-host': 'allsportsapi2.p.rapidapi.com',
     },
   };
 
   try {
     const response = await fetch(url, options);
     let result = await response.text();
-    //result = JSON.parse(result)
-    // console.log(JSON.parse(result, null, 2));
     const raceData = JSON.parse(result);
-    // console.log(raceData.races);
-    for (let i = 0; i < raceData.races.length - 1; i++) {
-      console.log(raceData.races[i]);
-      document.getElementById('teamraces').innerHTML = JSON.stringify(
-        raceData.races[i]
-      );
-    }
+    console.log(raceData);
 
-    //for (const data in JSON.stringify(result)) {
-    //console.log(data)
-    // document.getElementById('teamraces').innerHTML = 'RACE DATA GOES HERE';
-    //}
-    //document.getElementById("teamraces").innerHTML = result.races.map((item) => `<li${item.name}</li>`)
+    let str = '<ul>';
+
+    for (let i = 0; i < raceData.standings.length - 1; i++) {
+      console.log(raceData.standings[i]);
+      str += '<li>' + JSON.stringify(raceData.standings[i].team.name) + '</li>';
+    }
+    str += '</ul>';
+    document.getElementById('teamraces').innerHTML = str;
   } catch (error) {
     console.error(error);
   }
 }
 getSportsData();
+
+// JSON.stringify(
+// raceData.standings[i]
+
+// `<li${item}</li>`
+
+//document.getElementById('teamraces').innerHTML +=
